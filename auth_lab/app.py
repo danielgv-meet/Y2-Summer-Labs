07eac68b546341b9f100bb4e042baf9683112d73
danielgv-meet <startech5674@gmail.com>
2024-07-22 18:43:13 +0300

    excolator

--- auth_lab/app.py
+++ auth_lab/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask import session as login_session
 import pyrebase
+import firebase
+
 
 firebaseConfig = {
   "apiKey": "AIzaSyB057zr6FS31fUqXSuvPuGwTgf4CU3_MNM",
@@ -9,14 +11,17 @@ firebaseConfig = {
   "storageBucket": "auth-lab-1349a.appspot.com",
   "messagingSenderId": "916347876212",
   "appId": "1:916347876212:web:aa74c8b48d817af88fb221",
-  "databaseURL":""
+  "databaseURL":"https://auth-lab-1349a-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
 auth = firebase.auth()
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 app.config['SECRET_KEY'] = 'super-secret-key'
+
+
 
 @app.route("/", methods=['GET', 'POST'])
 def sign_up():
@@ -25,13 +30,20 @@ def sign_up():
   else:
     email = request.form['email']
     password = request.form['password']
+    username = request.form['username']
+    full_name = request.form['fullname']
+    user = {"full_name": full_name, "username": username, "email": email}
     try:
       login_session['user'] = auth.create_user_with_email_and_password(email, password)
+      UID = login_session["user"]["localId"]
+      db.child("Users").child(UID).set(user)
+      # print(db.child("Users").child(UID).get().val())
+      return redirect(url_for("home"))
     except:
       print("something went wrong")
-    login_session["quotes"] = [] 
-    return redirect("/home")
-    
+      return render_template("error.html", error="The user already exist")     
+  
+   
 
 
 @app.route("/signin", methods=['GET', 'POST'])
@@ -40,24 +52,32 @@ def sign_in():
     print("GET")
     return render_template("signin.html")
   else:
-    login_session["email"] = request.form["email"]
-    login_session["password"] = request.form['password']
-    login_session["quotes"] = []
-    return redirect(url_for("home"))
+    email = request.form["email"]
+    password = request.form['password']
+    try:
+      login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+      return redirect(url_for("home"))
+    except:
+      print("The user is not existing")
+      return render_template("error.html", error = "the user does not exist")
+    
 
 @app.route("/signout")
 def signout():
   login_session['user'] = None
   auth.current_user = None
+  login_session["quotes"] = []
   return redirect(url_for('sign_in'))
+
 
 @app.route("/home", methods=["POST", "GET"])
 def home():
   if request.method == "GET":
     return render_template("home.html")
+
   else:
-    login_session["quotes"].append(request.form["quote"])
-    login_session.modified = True
+    quotes = {"text": request.form["quote"], "said_by": request.form["teller"], "uid": login_session['user']["localId"]}
+    db.child("Quotes").push(quotes)
     return redirect(url_for('thanks'))
 
 
@@ -67,7 +87,8 @@ def thanks():
 
 @app.route("/display")
 def display():
-  return render_template("display.html", quotes = login_session["quotes"])
+  return render_template("display.html", quotes = db.child("Quotes").get().val())
+
 
 if __name__ == "__main__":
   app.run(debug=True)
